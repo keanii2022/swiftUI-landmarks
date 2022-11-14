01070605db5681cb248efd77d475eb6b4455ceeb
keanii2018 <keani.mpcc@outlook.com>
2022-11-14 14:09:34 -0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Keani Antezana on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
