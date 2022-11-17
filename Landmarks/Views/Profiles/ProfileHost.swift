2580e400fa627d89d63dc7e7c13e03baf4ad5bad
keanii2018 <keani.mpcc@outlook.com>
2022-11-17 11:01:47 -0800

    profiles UI started

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Keani Antezana on 11/17/22.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        Text("Profile for: \(draftProfile.username)")
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
