cab9f50bb83530f1b55049fd3a313aa939daa110
keanii2018 <keani.mpcc@outlook.com>
2022-11-17 14:03:02 -0800

    Interfacing with UIKit

---
+++ Landmarks/Views/FeatureCard.swift
@@ -0,0 +1,24 @@
+//
+//  FeatureCard.swift
+//  Landmarks
+//
+//  Created by Keani Antezana on 11/17/22.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        landmark.featureImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard(landmark: ModelData().features[0])
+    }
+}
